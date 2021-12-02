718245460e6de5b5fc6a1650d23c4c0a8bf42af7
Lee Siang Yin <siangyin916@gmail.com>
2021-12-02 22:01:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5050;
 const bodyParser = require("body-parser");
-const drinks = require(__dirname + "/models/drinks.js")
-const foods = require(__dirname + "/models/foods.js")
+const drinks = require(__dirname + "/models/drinks.js");
+const foods = require(__dirname + "/models/foods.js");
 
 //basic ejs setup with express
 const app = express();
@@ -12,28 +12,27 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
- res.send("😀 Welcome to GitPub");
+	res.send("😀 Welcome to GitPub");
 });
 
 //drinks
 app.get("/drinks", function (req, res) {
-  res.render("index",{list:drinks,title:"drinks"});
+	res.render("index", { list: drinks, title: "drinks" });
 });
 
 // setup dynamic route for each drinks
-app.get ("/drinks/:id", function (req, res){
- res.render("show",{item:drinks[req.params.id]});
+app.get("/drinks/:id", function (req, res) {
+	res.render("show", { item: drinks[req.params.id] });
 });
 
 app.get("/foods", function (req, res) {
-  res.render("index",{list:foods,title:"foods"});
+	res.render("index", { list: foods, title: "foods" });
 });
 
-app.get ("/foods/:id", function (req, res){
- res.render("show",{item:foods[req.params.id]});
+app.get("/foods/:id", function (req, res) {
+	res.render("show", { item: foods[req.params.id] });
 });
 
-
-app.listen(port, function(){
-  console.log("Server started on port 5000.");
+app.listen(port, function () {
+	console.log("Server started on port 5000.");
 });
